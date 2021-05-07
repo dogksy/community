4e154d659c0729f679c48bd0820d5a896732b226
dogsky <1017578189@qq.com>
2021-05-07 10:18:27 +0800

    测试提交

---
+++ src/main/java/com/zty/community/controller/pushController.java
@@ -0,0 +1,10 @@
+package com.zty.community.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class pushController {
+    public String push(){
+        return "push成功";
+    }
+}
