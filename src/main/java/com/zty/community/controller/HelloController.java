bce7074dcdf99aaa9ebdc44b53debe7beddb632e
dogsky <1017578189@qq.com>
2021-05-27 16:34:39 +0800

    删除flyway无用代码

--- src/main/java/com/zty/community/controller/HelloController.java
+++ src/main/java/com/zty/community/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.zty.community.controller;
 
 import com.zty.community.service.LoginService;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +16,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(HttpServletRequest request) {
         loginService.loginVerify(request);
-        Flyway flyway = Flyway.configure().dataSource("","","").load();
-        flyway.migrate();
         return "hello";
     }
 }
